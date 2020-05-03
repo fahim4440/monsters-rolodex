d3236cb2995194a7a16ef80ce913d58df46f47b6
fahim4440 <ahmadul244@gmail.com>
2020-05-03 23:59:29 +0600

    Updates

---
+++ precache-manifest.daea6d8ce716a3ff3711b070e416c7a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "549d3dc10efa0d4797c04a51f55182df",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "581ce27fe2b457b9a63e",
+    "url": "/monsters-rolodex/static/css/main.38fabc73.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "581ce27fe2b457b9a63e",
+    "url": "/monsters-rolodex/static/js/main.cbc35904.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
